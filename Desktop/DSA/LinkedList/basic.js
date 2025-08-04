17b1fa86cf5650e64c2e5dec9cc3ca2ad574794e
Anjali Patil <anjalipatil3993@gmail.com>
2025-08-04 19:55:29 +0530

    linklist-insertbtw, search, remove, reverse

--- Desktop/DSA/LinkedList/basic.js
+++ Desktop/DSA/LinkedList/basic.js
@@ -56,3 +56,130 @@ list.prepend(30);
 list.prepend(20);
 list.prepend(10);
 list.printlist();
+
+
+//3.............append the value (add the node at the end )
+append(value){
+    const newNode = new Node(value)
+    if(!this.head){
+        this.head = newNode
+    }else{
+        let curr = this.head
+        while(curr.next){
+            curr = curr.next
+        }
+        curr.next = newNode
+    }
+    this.size++
+}
+
+//4...............insert inbtw the two nodes
+    
+   insert(value,index){
+         if(index<0 || index>this.size){
+             return "invalid index"
+         }
+         if(index === 0){
+             this.prepend(value)
+         }else{
+             let newNode = new Node(value)
+             let prev = this.head
+             for(let i=0; i<index-1; i++){
+                 prev = prev.next
+             }
+             newNode.next = prev.next
+             prev.next = newNode
+             this.size++
+         }
+         
+     }
+
+
+ //5..............remove the node from begning (index = 0) and from middle or end (index>0)
+    
+     RemoveFrom(index){
+         if(index<0 || index>= this.size){
+             return null
+         }
+         let removedNode
+         if(index === 0){
+             removedNode = this.head
+             this.head = this.head.next 
+         }else{
+             let prev = this.head
+             for(let i=0; i<index-1; i++){
+                 prev = prev.next
+             }
+             removedNode = prev.next
+             prev.next = removedNode.next
+         }
+         this.size--
+         return removedNode.value
+         
+     }
+
+
+
+//6...............remove the value from beging or middle or end 
+  
+    Removevalue(value){
+      if(!this.head){
+          return "list is empty"
+      }
+      if(this.head.value === value){
+          this.head = this.head.next
+          this.size--
+          return `${value} removed`
+      }
+      let prev = this.head
+      while(prev.next && prev.next.value !== value){
+          prev = prev.next
+      }
+      if(prev.next){
+          prev.next = prev.next.next
+          this.size--
+          return`${value}removed`
+      }
+      return `${value} is not found in list`
+  }
+     
+
+//7.............find the node for its given value(searching)
+  //method:1 ===findnode(value) , return the node or null
+  findNode(value){
+    let curr = this.head
+    while(curr){
+        if(curr.value === value){
+            return curr
+        }
+        curr = curr.next
+    }
+    return null
+  }
+
+  //method:2===findIndex(value), return the index or -1
+  findIndex(value){
+    let curr = this.head
+    let index = 0
+    while(curr){
+        if(curr.value === value){
+            return index
+        }
+        curr = curr.next
+        index++
+    }
+    return -1
+  }
+
+//8............reverse the list
+  reverse(){
+     let prev = null
+     let curr = this.head
+     while(curr){
+         let next = curr.next
+         curr.next = prev
+         prev = curr
+         curr = next
+     }
+     this.head = prev
+  }
